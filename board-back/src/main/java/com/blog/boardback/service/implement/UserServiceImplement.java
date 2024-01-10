00a2d3fa99f3bf24a7cf6c5de9ab55299936ce72
heetaelee <josepheelee@gmail.com>
2024-01-10 22:59:14 +0900

    유저 정보 불러오기 API

---
+++ board-back/src/main/java/com/blog/boardback/service/implement/UserServiceImplement.java
@@ -0,0 +1,37 @@
+package com.blog.boardback.service.implement;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
+import com.blog.boardback.dto.response.ResponseDto;
+import com.blog.boardback.dto.response.user.GetSignInUserResponseDto;
+import com.blog.boardback.entity.UserEntity;
+import com.blog.boardback.repository.UserRepository;
+import com.blog.boardback.service.UserService;
+
+import lombok.RequiredArgsConstructor;
+
+@Service
+@RequiredArgsConstructor
+public class UserServiceImplement implements UserService {
+
+  private final UserRepository userRepository;
+
+  @Override
+  public ResponseEntity<? super GetSignInUserResponseDto> getSignInUser(String email) {
+
+    UserEntity userEntity = null;
+
+    try {
+      userEntity = userRepository.findByEmail(email);
+      if (userEntity == null) return GetSignInUserResponseDto.notExistUser();
+
+    } catch(Exception exception) {
+      exception.printStackTrace();
+      return ResponseDto.databaseError();
+    }
+
+    return GetSignInUserResponseDto.success(userEntity);
+
+  }
+
+}

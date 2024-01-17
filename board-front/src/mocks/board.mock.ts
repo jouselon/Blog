2f382cdbecf9362881d9dec4afcaa5f39dd95d36
heetaelee <josepheelee@gmail.com>
2024-01-18 00:02:20 +0900

    게사물 상세 페이지 이벤트 처리

---
+++ board-front/src/mocks/board.mock.ts
@@ -0,0 +1,14 @@
+import {Board} from "types/interface"
+
+const boardMock: Board = {
+    boardNumber : 1,
+    title : '오늘은 뭐먹지?',
+    content: '고민고민하지마',
+    boardImageList: ['https://steemitimages.com/DQmXZ6Vbn3C5hB2WqpZK3KzrKJBf9u84W4MsfVhJkYYQ6DC/songdogukjedosi%20night%20view.jpg','https://cdn.crowdpic.net/list-thumb/thumb_l_F25C5FD45B78842BE8B499E04852D8CB.jpg'],
+    writeDatetime : '2022.05.12',
+    writerEmail : 'email@email',
+    writerNickname: '리효리',
+    writerProfileImage: 'https://image.utoimage.com/preview/cp872722/2022/12/202212008462_500.jpg',
+}
+
+export default boardMock;
--- board-front/src/types/interface/index.ts
+++ board-front/src/types/interface/index.ts
@@ -2,12 +2,16 @@ import User from "../../views/User";
 import BoardListItem from "./board-list-item.interface";
 import FavoriteListItem from "./favorite-list-item.interface";
 import CommentListItem from "./comment-list-item.interface";
+import Board from "./board.interface";
+import BoardMock from "../../mocks/board.mock";
 
 export type {
     User,
+    Board,
     BoardListItem,
     FavoriteListItem,
-    CommentListItem
+    CommentListItem,
+    BoardMock,
 
 
 }
